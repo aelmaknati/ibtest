7aa99f5841b210e230ea02a7e1f1124c558d616b
aelmaknati <abdellatif.el-maknati.ext@bureauveritas.com>
2023-01-02 11:01:32 +0100

    Passage en sh

--- tests/unit/services/CalculatriceService.test.js
+++ tests/unit/services/CalculatriceService.test.js
@@ -85,12 +85,6 @@ describe('CalculatriceService', function() {
         assert.equal(spy.callCount , 2);
         return done();
       })
-
-      it('should fail', function (done) {
-
-        assert.equal(5 , 2);
-        return done();
-      })
     });
 
 
